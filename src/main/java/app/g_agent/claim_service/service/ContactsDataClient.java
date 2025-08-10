62282768038e93349d9f84b5a0a86187f1dfc883
Ndiithi <duncanndiithi@yahoo.com>
2025-08-10 20:48:37 +0300

    http clients

---
+++ src/main/java/app/g_agent/claim_service/service/ContactsDataClient.java
@@ -0,0 +1,29 @@
+package app.g_agent.claim_service.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+//import AdministrativeAreaDto
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import app.g_agent.claim_service.dto.ContactAddressWrapper;
+import app.g_agent.claim_service.dto.ContactDto;
+import app.g_agent.claim_service.dto.UserDto;
+
+@FeignClient(name = "contact-service")
+public interface ContactsDataClient {
+
+    @GetMapping("/contact-service/api/v1/contact/get-contacts-by-ids")
+    ContactAddressWrapper getContactsByIds(@RequestParam String contactIds,
+            @RequestHeader Map<String, String> headers);
+
+    @PostMapping("/contact-service/api/v1/contact/search")
+    List<ContactDto> getContactsByKeyword(@RequestParam String keyword,
+            @RequestHeader Map<String, String> headers);
+}
---
+++ src/main/java/app/g_agent/claim_service/service/UsersDataClient.java
@@ -0,0 +1,22 @@
+package app.g_agent.claim_service.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+//import AdministrativeAreaDto
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import app.g_agent.claim_service.dto.UserDto;
+
+@FeignClient(name = "user-service")
+public interface UsersDataClient {
+
+    @GetMapping("/user-service/api/v1/user/get-contacts-by-ids")
+    List<UserDto> getUsersByIds(@RequestParam String ids,
+            @RequestHeader Map<String, String> headers);
+}
